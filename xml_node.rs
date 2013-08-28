2bd2a3f844e29e265d0c000dc0593598332fff8f
DanielFath <daniel.fath7@gmail.com>
2013-08-28 20:09:10 +0200

    Moved Impl to end of enum/struct declarations
    - Trying to be more in line with https://github.com/mozilla/rust/wiki/Note-style-guide

--- xml_node.rs
+++ xml_node.rs
@@ -1,19 +1,10 @@
-
-
-// General types
 #[deriving(Clone,Eq)]
-/// An Enum describing a XML Node
-pub enum XmlNode {
-    /// An XML Element
-    XmlElem(~XmlElem),
-    /// Character Data
-    XmlText(~str),
-    /// CDATA
-    XmlCDATA(~str),
-    /// A XML Comment
-    XmlComment(~str),
-    /// Processing Information
-    PINode(~PINode)
+/// A struct representing an XML root document
+pub struct XmlDoc {
+    // The document's root
+    root: ~XmlElem,
+    // The document's processing instructions
+    pi: ~[PINode]
 }
 
 #[deriving(Clone,Eq)]
@@ -25,13 +16,6 @@ pub struct PINode {
     /// Must not contain ?>
     value: ~str
 }
-
-impl PINode {
-    pub fn to_str(&self) -> ~str {
-       fmt!("<?%s %s ?>", self.target, self.value)
-    }
-}
-
 
 #[deriving(Clone,Eq)]
 /// A struct representing an XML element
@@ -46,37 +30,6 @@ pub struct XmlElem {
     children: ~[XmlNode]
 }
 
-
-#[deriving(Clone,Eq)]
-/// A struct representing an XML root document
-pub struct XmlDoc {
-    // The document's root
-    root: ~XmlElem,
-    // The document's processing instructions
-    pi: ~[PINode]
-}
-
-impl XmlDoc {
-    pub fn new() -> XmlDoc {
-        XmlDoc {
-            root: ~XmlElem {
-                    name:~"",
-                    namespace:~XmlNS{name: ~"", uri: ~""}, 
-                    attributes: ~[],
-                    children: ~[]
-            },
-            pi: ~[]
-        }
-    }
-
-    pub fn to_str(&self) -> ~str {
-        let mut ret = ~"";
-        for e in self.pi.iter() {
-            ret = ret + e.to_str();
-        }
-        ret
-    }
-}
 
 #[deriving(Clone,Eq)]
 /// A struct representing an XML attribute
@@ -98,11 +51,23 @@ pub struct XmlNS {
     uri: ~str
 }
 
-impl XmlNS {
-    pub fn to_str() -> ~str {
-        ~""
-    }
+
+// General types
+#[deriving(Clone,Eq)]
+/// An Enum describing a XML Node
+pub enum XmlNode {
+    /// An XML Element
+    XmlElem(~XmlElem),
+    /// Character Data
+    XmlText(~str),
+    /// CDATA
+    XmlCDATA(~str),
+    /// A XML Comment
+    XmlComment(~str),
+    /// Processing Information
+    PINode(~PINode)
 }
+
 
 #[deriving(Eq)]
 /// Events returned by the Parser
@@ -134,6 +99,41 @@ fn main() {
     
 }
 
+impl XmlDoc {
+    pub fn new() -> XmlDoc {
+        XmlDoc {
+            root: ~XmlElem {
+                    name:~"",
+                    namespace:~XmlNS{name: ~"", uri: ~""}, 
+                    attributes: ~[],
+                    children: ~[]
+            },
+            pi: ~[]
+        }
+    }
+
+    pub fn to_str(&self) -> ~str {
+        let mut ret = ~"";
+        for e in self.pi.iter() {
+            ret = ret + e.to_str();
+        }
+        ret
+    }
+}
+
+
+impl PINode {
+    pub fn to_str(&self) -> ~str {
+       fmt!("<?%s %s ?>", self.target, self.value)
+    }
+}
+
+
+impl XmlNS {
+    pub fn to_str() -> ~str {
+        ~""
+    }
+}
 
 #[test]
 fn test_pi(){
