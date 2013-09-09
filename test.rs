305ecfb7d3290100143f617304625bd0f837d637
DanielFath <daniel.fath7@gmail.com>
2013-09-09 21:50:08 +0200

    Commited tests for time being

--- test.rs
+++ test.rs
@@ -1,20 +1,38 @@
-use std::io::{with_str_reader,Reader};
+use std::io::{BytesReader,Reader, ReaderUtil};
 
 pub struct ReadSource<'self> {
-    priv source: &'self Reader
+    source: @Reader,
+    priv do_nothing: &'self str
 }
 
 impl<'self> ReadSource<'self> {
+/*
+    fn from_str(data: ~str) -> ReadSource<'self>{
+        let r = @BytesReader {
+                bytes : data.as_bytes(),
+                pos:  @mut 0
+        } as @Reader;
 
-    fn from_str(data: &'self str) -> ReadSource<'self>{
-        let r = std::io::with_str_reader(data, | reader| { reader });
-        ReadSource{
-            source : r
+        ReadSource {
+            source :  r, 
+            do_nothing : ""
         }
+    }*/
+
+    fn next(&mut self){
+        self.source.read_char();
     }
 }
 
 fn main() {
+    let r = @BytesReader {
+                bytes : "as".as_bytes(),
+                pos: @mut 0
+    } as @Reader;
 
-    
+    let rs = ReadSource {
+        source : r, do_nothing : ""
+    };
+
+    rs.source.read_char();
 }
