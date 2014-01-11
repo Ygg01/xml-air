e33d880d1e4db1899e914bc6feac9bbecc8e60fe
DanielFath <daniel.fath7@gmail.com>
2014-01-12 00:28:23 +0100

    Test stuff

--- src/xml-parser/parser_example.rs
+++ src/xml-parser/parser_example.rs
@@ -1,11 +1,41 @@
-pub use xml_parser::*;
-pub use xml_node::*;
+// Simple struct that is mutated to get next value
+pub struct Obj {
+    stuff: uint
+}
 
-pub mod xml_parser;
-pub mod xml_node;
+impl<'a> Obj {
+    // Necessary mutating function
+    pub fn decr(&mut self) {
+        self.stuff -= 1;
+    }
+    #[inline]
+    pub fn iterate(&'a mut self) -> ObjIterator<'a> {
+        ObjIterator{iter: self}
+    }
+}
+
+// Struct to help with the Iterator pattern emulating Rust native libraries
+pub struct ObjIterator <'b> {
+    priv iter: &'b mut Obj
+}
+
+// The problem seems to be here
+impl<'b> Iterator<uint> for ObjIterator<'b> {
+    // Apparently I can't have &'b mut
+    fn next(&mut self) -> Option<uint> {
+        self.iter.decr();
+        // Pointless effects to prevent infinite loop on execution
+        if(self.iter.stuff == 0){
+            None
+        }else {
+            Some(self.iter.stuff)
+        }
+    }
+}
 
 fn main() {
-    let parser = Parser::new();
-    let xmlStr = ~"<root></root>";
-    parser.parse_str(xmlStr);
+    let mut obj = Obj{stuff: 3};
+    for c in obj.iterate() {
+        println!("Got {}", c);
+    }
 }
