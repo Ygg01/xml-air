2124868901c8627fd302877ed07c6bbbc8b953a0
DanielFath <daniel.fath7@gmail.com>
2014-02-04 21:41:44 +0100

    Add more Character reference test cases
    - Add decimal and hexadecimal versions of chars

--- src/xml/lexer.rs
+++ src/xml/lexer.rs
@@ -1458,7 +1458,7 @@ mod tests {
 
     #[test]
     fn test_element(){
-        let str1  = bytes!("<elem attr='something &ref;bla'></elem>");
+        let str1  = bytes!("<elem attr='something &ref;bla&#35;&#x2A;'></elem>");
         let read1 = BufReader::new(str1);
 
         let mut lexer = Lexer::from_reader(read1);
@@ -1471,6 +1471,8 @@ mod tests {
         assert_eq!(Some(Text(~"something ")),   lexer.pull());
         assert_eq!(Some(Ref(~"ref")),           lexer.pull());
         assert_eq!(Some(Text(~"bla")),          lexer.pull());
+        assert_eq!(Some(CharRef('#')),          lexer.pull());
+        assert_eq!(Some(CharRef('*')),          lexer.pull());
         assert_eq!(Some(Quote),                 lexer.pull());
         assert_eq!(Some(GreaterBracket),        lexer.pull());
     }
