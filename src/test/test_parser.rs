4ebf4f16f6bf1d400e47d08939925b9f922daf8e
Daniel Fath <daniel.fath7@gmail.com>
2014-04-29 02:18:34 +0200

    Improve test
    - Add more test cases for an element
    - Add test for empty elements

--- src/test/test_parser.rs
+++ src/test/test_parser.rs
@@ -8,6 +8,17 @@ fn test_element(){
     let str1 = bytes!("<a>");
     let mut reader = BufReader::new(str1);
     let mut parser = Parser::from_reader(&mut reader);
+    let a = XmlElem::new("a");
 
     assert_eq!(Some(ElemStart),     parser.pull());
+    assert_eq!(Some(a),             parser.elem);
+}
+
+#[test]
+fn test_empty(){
+    let str1 = bytes!("");
+    let mut reader = BufReader::new(str1);
+    let mut parser = Parser::from_reader(&mut reader);
+
+    assert_eq!(None,    parser.pull());
 }
